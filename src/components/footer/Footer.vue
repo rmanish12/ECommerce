f8be3ebe77d9670ee2f535ad6489a6caa28df815
Ranjan <manish.ranjan@metronom-external.com>
2018-12-25 14:58:39 +0530

    first commit

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- <div class="footer">
+    <div class="columns container">
+      <div class="column">
+        <p>Footer for the site</p>
+      </div>
+    <div class="column has-text-right">
+      <p>.....</p>
+    </div>
+    </div>
+  </div> -->
+  <footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      Footer component where we can put copyright or terms and conditions or something similar.
+    </p>
+  </div>
+</footer>
+</template>
+
+<script>
+export default {
+  name: 'footer-component',
+}
+</script>
+
+<style lang="scss" scoped>
+ .footer {
+   background: #b9e2fc;
+   color: black;
+ }
+</style>
---
+++ src/components/hero/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="hero is-medium is-light is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            An ecommerce Website
+          </h1>
+          <h2 class="subtitle">
+            Here we can write something about the website or something else..
+          </h2>
+        </div>
+      </div>
+    </section>   
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+      background: rgb(136, 49, 49);
+    }
+    h1, h2 {
+      color: #fff;
+    }
+</style>
